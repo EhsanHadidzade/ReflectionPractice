a32a77e76b259cd15aa66e24828eeea40d83e0bc
unknown <ehsan.hd.1996@gmail.com>
2023-11-09 15:51:00 +0330

    second commit

---
+++ ReflectionPractice.App/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+ 
+using System.Reflection;
+using Utilities;
+
+var assembly = Assembly.Load("Utilities");
+var assemblyTypes= assembly.GetTypes();
+//var assembly = Assembly.LoadFrom(@"D:\MySamples\ReflectionPractice\Utilities\bin\Debug\net7.0\Utilities.dll");
+
+
+
+foreach (var type in assemblyTypes)
+{
+   if (type.Name == "VandarService")
+    {
+        Console.WriteLine(type.Name);
+        Console.WriteLine("================");
+
+        var properties = type.GetProperties();
+        var fields = type.GetFields(BindingFlags.NonPublic
+                                            | BindingFlags.Instance
+                                            | BindingFlags.DeclaredOnly);
+
+        var methods=type.GetMethods(BindingFlags.Public | BindingFlags.Instance );
+        var methodsWithDeclared=type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly );
+
+        foreach (var field in fields)
+        {
+            Console.WriteLine($"field is {field.Name}");
+        }
+
+
+        foreach (var method in methodsWithDeclared)
+        {
+            Console.WriteLine($"method is {method.Name}");
+        }
+
+        foreach (var property in properties)
+        {
+            if (property.PropertyType.IsEnum)
+                Console.WriteLine($"my enum is {property.Name}");
+
+            else
+                Console.WriteLine($"property : {property.Name}");
+        }
+    }
+
+
+
+
+}
---
+++ Utilities/VandarService.cs
@@ -0,0 +1,34 @@
+﻿namespace Utilities
+{
+    public class VandarService
+    {
+        private string vandarToken;
+        public paymentMethod PaymentMethod { get; set; }
+        public string GetGateWayToken(int orderId)
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public void payOrder(int orderId)
+        {
+            Console.WriteLine($"order with id of{orderId} paid ");
+        }
+
+        public void PrintOrderDetails(int orderId)
+        {
+            Console.WriteLine($"order with id of {orderId} details printed");
+        }
+
+        private bool orderValidation(int orderId)
+        {
+            return true;
+        }
+    }
+
+    public enum paymentMethod
+    {
+        None = 0,
+        online=1,
+        wallet=2
+    }
+}
